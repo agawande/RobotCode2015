0037ef0834ff1aab2032e177365373316b3fcf0e
ashu <agawande@memphis.edu>
2015-01-08 18:12:09 -0600

    Added files for motor control

---
+++ motor-control/MotorControl.cpp
@@ -0,0 +1,111 @@
+/*
+MotorControl.cpp Library for controlling a two wheel robot (i.e. control two motors)
+Created by Ashlesh, Dec 19, 2014
+*/
+
+#include "Arduino.h"
+#include "MotorControl.h"
+
+
+MotorControl::MotorControl(int ENA, int ENB, int IN1, int IN2, int IN3, int IN4)
+{
+	 //wheel 1
+	 pinMode(ENA, OUTPUT);     // Define as an output
+	 pinMode(IN1, OUTPUT);     // Define as an output
+	 pinMode(IN2, OUTPUT);     // Define as an output
+	 
+	 //wheel 2
+	 pinMode(ENB, OUTPUT);     // Define as an output
+	 pinMode(IN3, OUTPUT);     // Define as an output
+	 pinMode(IN4, OUTPUT);     // Define as an output
+	 
+	_ENA = ENA;
+	_ENB = ENB;
+	_IN1 = IN1;
+	_IN2 = IN2;
+	_IN3 = IN3;
+	_IN4 = IN4;
+}
+
+void MotorControl::accelerateForward()
+{
+ digitalWrite(_IN1, HIGH);     // Sets motor to run forward
+ digitalWrite(_IN2, LOW);     // Sets motor to run forward
+ 
+ digitalWrite(_IN3, HIGH);     // Sets motor to run forward
+ digitalWrite(_IN4, LOW);     // Sets motor to run forward
+
+ int i = 0;     // Set "i" to zero
+ while(i < 255)     //Accelerate motor over time
+ {
+ i+=5;
+ //Serial.println(i);       // Display current motor speed
+ analogWrite(_ENA, i);     // Send current motor speed setting to motor
+ analogWrite(_ENB, i);
+ delay(30);     // Wait
+ }
+}
+
+void MotorControl::driveForward()
+{
+ digitalWrite(_IN1, HIGH);     // Sets motor to run forward
+ digitalWrite(_IN2, LOW);     // Sets motor to run forward
+ analogWrite(_ENA, 255);      // Turns the motor on and sets speed to 255
+ 
+ digitalWrite(_IN3, HIGH);     // Sets motor to run forward
+ digitalWrite(_IN4, LOW);     // Sets motor to run forward
+ analogWrite(_ENB, 255);      // Turns the motor on and sets speed to 255
+} 
+
+void MotorControl::accelerateBack()
+{
+ digitalWrite(_IN1, LOW);     // Sets motor to run forward
+ digitalWrite(_IN2, HIGH);     // Sets motor to run forward
+ 
+ digitalWrite(_IN3, LOW);     // Sets motor to run forward
+ digitalWrite(_IN4, HIGH);     // Sets motor to run forward
+
+int i = 0;     // Set "i" to zero
+while(i < 255)     //Accelerate motor over time
+ {
+ i+=5;
+ //Serial.println(i);       // Display current motor speed
+ analogWrite(_ENA, i);     // Send current motor speed setting to motor
+ analogWrite(_ENB, i);
+ delay(30);     // Wait
+ }
+}
+
+void MotorControl::driveBack()
+{
+   digitalWrite(_IN1, LOW);     // Sets motor to run forward
+   digitalWrite(_IN2, HIGH);     // Sets motor to run forward
+   analogWrite(_ENA, 255);      // Turns the motor on and sets speed to 255
+   
+   digitalWrite(_IN3, LOW);     // Sets motor to run forward
+   digitalWrite(_IN4, HIGH);     // Sets motor to run forward
+   analogWrite(_ENB, 255);      // Turns the motor on and sets speed to 255
+}
+
+void MotorControl::slowStop()
+{
+ if(analogRead(_ENA) != 0 || analogRead(_ENB) != 0 )
+ {
+   for(int i=255; i>=0; i-=5) 
+   {
+	 //Serial.println(i);
+	 analogWrite(_ENA, i);
+	 analogWrite(_ENB, i);
+	 delay(30);
+   }
+ }
+}
+
+void MotorControl::halt()
+{
+ if(analogRead(_ENA) != 0 || analogRead(_ENB) != 0 )
+ {
+  analogWrite(_ENA, 0);
+  analogWrite(_ENB, 0);
+ }
+}
---
+++ motor-control/MotorControlUsinLib.ino
@@ -0,0 +1,65 @@
+#include "MotorControl.h"
+
+const int ENA = 10;    // Define the pin used on the Arduino
+const int ENB = 9;
+const int IN1 = 2;    // Define the pin used on the Arduino
+const int IN2 = 3;    // Define the pin used on the Arduino
+const int IN3 = 4;
+const int IN4 = 5;
+
+MotorControl robotMotor(10,9,2,3,4,5);
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+void setup()
+{
+ Serial.begin(9600);     // Set serial comm speed
+ Serial.println("Motor Test");     // Print a label for data to be displayed in serial terminal
+ inputString.reserve(200);
+}
+
+void loop()
+{
+  if (stringComplete) {
+    Serial.println(inputString); 
+    if(inputString == "f")
+    {
+      robotMotor.accelerateForward();
+      robotMotor.driveForward();
+    }
+    else if(inputString == "s")
+    {      
+      robotMotor.slowStop();
+    }
+    else if(inputString == "b")
+    {
+      robotMotor.accelerateBack();
+      robotMotor.driveBack();
+    }
+    else if(inputString == "h")
+    {
+      robotMotor.halt();
+    }
+    
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+}
+///////////////////////////////////////////////////////////////////////////
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read(); 
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it
+    // f = accelerate and forward, b = accelerate and backward, s = decelarate to stop, h=halt
+    if (inChar == 'f' || inChar == 'b' || inChar == 's' || inChar == 'h') {
+      stringComplete = true;
+    } 
+  }
+}
