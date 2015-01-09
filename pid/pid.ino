f5f15423dd6ab77bc83097c797cc8b1e895029df
Shawn Westcott <slwstctt@memphis.edu>
2015-01-09 16:16:17 -0600

    Don't need full quadrature
    Now we can read from both motors. Woo.

--- pid/pid.ino
+++ pid/pid.ino
@@ -1,86 +1,55 @@
 
 #include "Arduino.h"
-#include "digitalWriteFast.h"  // library for high performance reads and writes by jrraines
-                               // see http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1267553811/0
-                               // and http://code.google.com/p/digitalwritefast/
-
-// It turns out that the regular digitalRead() calls are too slow and bring the arduino down when
-// I use them in the interrupt routines while the motor runs at full speed.
 
 // Quadrature encoders
 // Left encoder
-#define c_LeftEncoderInterruptA 0
-#define c_LeftEncoderInterruptB 1
-#define c_LeftEncoderPinA 2
-#define c_LeftEncoderPinB 3
-#define LeftEncoderIsReversed
 
-volatile bool _LeftEncoderASet;
-volatile bool _LeftEncoderBSet;
-volatile bool _LeftEncoderAPrev;
-volatile bool _LeftEncoderBPrev;
+#define c_RightEncoderInterrupt 0
+#define c_LeftEncoderInterrupt 1
+#define c_RightEncoderPin 2
+#define c_LeftEncoderPin 3
+
 volatile long _LeftEncoderTicks = 0;
+volatile long _RightEncoderTicks = 0;
 
 void setup()
 {
-  Serial.begin(9600);
+	Serial.begin(9600);
 
-  // Quadrature encoders
-  // Left encoder
-  pinMode(c_LeftEncoderPinA, INPUT);      // sets pin A as input
-  digitalWrite(c_LeftEncoderPinA, LOW);  // turn on pullup resistors
-  pinMode(c_LeftEncoderPinB, INPUT);      // sets pin B as input
-  digitalWrite(c_LeftEncoderPinB, LOW);  // turn on pullup resistors
-  attachInterrupt(c_LeftEncoderInterruptA, HandleLeftMotorInterruptA, CHANGE);
-  attachInterrupt(c_LeftEncoderInterruptB, HandleLeftMotorInterruptB, CHANGE);
+	// Quadrature encoders
+	// Left encoder
+	pinMode(c_LeftEncoderPin, INPUT);			// sets pin as input
+	digitalWrite(c_LeftEncoderPin, LOW);		// turn on pullup resistors
+	attachInterrupt(c_LeftEncoderInterrupt, HandleLeftMotorInterrupt, CHANGE);
+
+	// Right encoder
+	pinMode(c_RightEncoderPin, INPUT);			// sets pin as input
+	digitalWrite(c_RightEncoderPin, LOW);		// turn on pullup resistors
+	attachInterrupt(c_RightEncoderInterrupt, HandleRightMotorInterrupt, CHANGE);
 }
 
 void loop()
 { 
-  Serial.print("Encoder Ticks: ");
-  Serial.print(_LeftEncoderTicks);
-  Serial.print("  Revolutions: ");
-  Serial.print(_LeftEncoderTicks/8400.0);//, how to count: 64 counts/rev (PDR) * 131.25:1 (gear ratio) = 8400
-  Serial.print("\n");
+	Serial.print("Left Encoder Ticks: ");
+	Serial.print(_LeftEncoderTicks);
+	Serial.print("  Left Revolutions: ");
+	Serial.print(_LeftEncoderTicks/4200.0);			// how to count: 64 counts/rev (PDR) * 131.25:1 (gear ratio) = 8400, Half resolution = 4200.
+	
+	Serial.print("  Right Encoder Ticks: ");
+	Serial.print(_RightEncoderTicks);
+	Serial.print("  Right Revolutions: ");
+	Serial.print(_RightEncoderTicks/4200.0);			// how to count: 64 counts/rev (PDR) * 131.25:1 (gear ratio) = 8400, Half resolution = 4200.
+	Serial.print("\n");
 }
 
-
 // Interrupt service routines for the left motor's quadrature encoder
-void HandleLeftMotorInterruptA(){
-  _LeftEncoderBSet = digitalReadFast(c_LeftEncoderPinB);
-  _LeftEncoderASet = digitalReadFast(c_LeftEncoderPinA);
-  
-  _LeftEncoderTicks+=ParseEncoder();
-  
-  _LeftEncoderAPrev = _LeftEncoderASet;
-  _LeftEncoderBPrev = _LeftEncoderBSet;
+void HandleLeftMotorInterrupt(){
+	// Test transition;
+	_LeftEncoderTicks++;
 }
 
 // Interrupt service routines for the right motor's quadrature encoder
-void HandleLeftMotorInterruptB(){
-  // Test transition;
-  _LeftEncoderBSet = digitalReadFast(c_LeftEncoderPinB);
-  _LeftEncoderASet = digitalReadFast(c_LeftEncoderPinA);
-  
-  _LeftEncoderTicks+=ParseEncoder();
-  
-  _LeftEncoderAPrev = _LeftEncoderASet;
-  _LeftEncoderBPrev = _LeftEncoderBSet;
+void HandleRightMotorInterrupt(){
+	// Test transition;
+	_RightEncoderTicks++;
 }
-
-int ParseEncoder(){
-  if(_LeftEncoderAPrev && _LeftEncoderBPrev){
-    if(!_LeftEncoderASet && _LeftEncoderBSet) return 1;
-    if(_LeftEncoderASet && !_LeftEncoderBSet) return -1;
-  }else if(!_LeftEncoderAPrev && _LeftEncoderBPrev){
-    if(!_LeftEncoderASet && !_LeftEncoderBSet) return 1;
-    if(_LeftEncoderASet && _LeftEncoderBSet) return -1;
-  }else if(!_LeftEncoderAPrev && !_LeftEncoderBPrev){
-    if(_LeftEncoderASet && !_LeftEncoderBSet) return 1;
-    if(!_LeftEncoderASet && _LeftEncoderBSet) return -1;
-  }else if(_LeftEncoderAPrev && !_LeftEncoderBPrev){
-    if(_LeftEncoderASet && _LeftEncoderBSet) return 1;
-    if(!_LeftEncoderASet && !_LeftEncoderBSet) return -1;
-  }
-}
-
